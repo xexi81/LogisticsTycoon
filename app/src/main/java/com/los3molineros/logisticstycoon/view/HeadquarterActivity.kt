ba464d9f3200196fcfd1ea5d1e7a4493884f3133
sergionunez <sergionunez90@gmail.com>
2021-08-05 19:46:00 +0200

    HeadquarterActivity.kt

--- app/src/main/java/com/los3molineros/logisticstycoon/view/HeadquarterActivity.kt
+++ app/src/main/java/com/los3molineros/logisticstycoon/view/HeadquarterActivity.kt
@@ -1,10 +1,12 @@
 package com.los3molineros.logisticstycoon.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.los3molineros.logisticstycoon.R
 import com.los3molineros.logisticstycoon.common.Companion
 import com.los3molineros.logisticstycoon.databinding.ActivityHeadquarterBinding
+import com.los3molineros.logisticstycoon.view.fragments.MoneyFragment
 
 class HeadquarterActivity : AppCompatActivity() {
     lateinit var binding: ActivityHeadquarterBinding
@@ -14,6 +16,13 @@ class HeadquarterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.moneyFragment, MoneyFragment())
+                .commitNow()
+        }
+
+
         binding.txtTittle.typeface = Companion.returnTypefaceKimbalt(this)
         binding.txtBasicWarehouse.typeface = Companion.returnTypefaceKingthings(this)
         binding.txtBasicWarehouse.typeface = Companion.returnTypefaceKingthings(this)
@@ -21,8 +30,16 @@ class HeadquarterActivity : AppCompatActivity() {
         binding.txtPrimayWarehouse.typeface = Companion.returnTypefaceKingthings(this)
         binding.txtSecondaryWarehouse.typeface = Companion.returnTypefaceKingthings(this)
         binding.txtFinalWarehouse.typeface = Companion.returnTypefaceKingthings(this)
+        binding.txtUpgradeGems.typeface = Companion.returnTypefaceKingthings(this)
+        binding.txtUpgradeMoney.typeface = Companion.returnTypefaceKingthings(this)
 
 
+
+        // Progress Bar
+        binding.txtLevel.text = "NIVEL 27"
+        binding.txtLevel.typeface = Companion.returnTypefaceKingthings(this)
+        binding.pbLevel.max = 500
+        binding.pbLevel.progress = 25
 
         // set on clicks
         binding.ivExit.setOnClickListener {
